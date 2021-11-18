019b2c6b381970c920bc9d99a107c866fe7553b5
BMann-png <bmann@student.neumont.edu>
2021-11-17 18:39:35 -0700

    Game Added

---
+++ SlayTheRooms/Game.cpp
@@ -0,0 +1,107 @@
+#include "Game.h"
+
+void Game::Initialize()
+{
+	//create engine
+	engine = std::make_unique<pbls::Engine>();
+	engine->Startup();
+	engine->Get<pbls::Renderer>()->Create("GAT150", 800, 600);
+	
+	//register components
+	
+
+
+	//create scene
+	scene = std::make_unique<pbls::Scene>();
+	scene->engine = engine.get();
+
+	pbls::SeedRandom(static_cast<unsigned int>(time(nullptr)));
+	pbls::SetFilePath("../Resources");
+
+	engine->Get<pbls::EventSystem>()->Subscribe("onAddScore", std::bind(&Game::OnAddScore, this, std::placeholders::_1));
+
+}
+
+void Game::Shutdown()
+{
+	scene->RemoveAllActors();
+	engine->Shutdown();
+}
+
+void Game::Update()
+{
+	engine->Update();
+
+	quit = (engine->Get<pbls::InputSystem>()->GetKeyState(SDL_SCANCODE_ESCAPE) == pbls::InputSystem::eKeyState::Pressed);
+
+	switch (state)
+	{
+	case Game::eState::Reset:
+		Reset();
+		break;
+	case Game::eState::Title:
+		Title();
+		break;
+	case Game::eState::StartGame:
+		StartGame();
+		break;
+	case Game::eState::StartLevel:
+		StartLevel();
+		break;
+	case Game::eState::Level:
+		Level();
+		break;
+	case Game::eState::PlayerDead:
+		PlayerDead();
+		break;
+	case Game::eState::GameOver:
+		GameOver();
+		break;
+	default:
+		break;
+	}
+
+	
+	scene->Update(engine->time.deltaTime);
+}
+
+void Game::Draw()
+{
+	
+
+}
+
+void Game::Reset()
+{
+	
+}
+
+void Game::Title()
+{
+	
+}
+
+void Game::StartGame()
+{
+	
+}
+
+void Game::StartLevel()
+{
+	
+}
+
+void Game::Level()
+{
+	
+}
+
+void Game::PlayerDead()
+{
+	
+}
+
+void Game::GameOver()
+{
+
+}
---
+++ SlayTheRooms/Game.h
@@ -0,0 +1,54 @@
+#pragma once
+#include "Engine.h"
+#include <iostream>
+#include <SDL.h>
+#include <SDL_Image.h>
+#include <cassert>
+
+class Game
+{
+public:
+	enum class eState
+	{
+		Reset,
+		Title,
+		StartGame,
+		StartLevel,
+		Level,
+		PlayerDead,
+		GameOver
+	};
+
+public:
+	void Initialize();
+	void Shutdown();
+
+	void Update();
+	void Draw();
+
+	bool IsQuit() { return quit; }
+
+private:
+	void Reset();
+	void Title();
+	void StartGame();
+	void StartLevel();
+	void Level();
+	void PlayerDead();
+	void GameOver();
+
+	void OnAddScore(const pbls::Event& event);
+
+public:
+	std::unique_ptr<pbls::Engine> engine;
+	std::unique_ptr<pbls::Scene> scene;
+
+private:
+	bool quit{ false };
+
+	eState state = eState::Reset;
+	int score{ 0 };
+	float stateTimer{ 0 };
+	float spawnTimer{ 0 };
+
+};
